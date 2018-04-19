6ee75c01fc845078885951b3d8aab9ea3525c10e
codarsan <@eclipse>
2018-04-19 12:32:24 +0100

    sqd

--- src/main/java/csan/springframework/DependencyInjectionTestApplication.java
+++ src/main/java/csan/springframework/DependencyInjectionTestApplication.java
@@ -2,11 +2,15 @@ package csan.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import csan.springframework.controllers.HelloController;
 
 @SpringBootApplication
 public class DependencyInjectionTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionTestApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DependencyInjectionTestApplication.class, args);
+		HelloController contr = (HelloController) ctx.getBean("helloController");
+		contr.Hello();
 	}
 }
---
+++ src/main/java/csan/springframework/controllers/HelloController.java
@@ -0,0 +1,11 @@
+package csan.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+	public void Hello () {
+		System.out.println("Hellow Word !!");
+	}
+
+}
