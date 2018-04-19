0fa69d277d25fb9876233466a22b1274914d4ddc
codarsan <37899107+codarsan@users.noreply.github.com>
2018-04-19 12:08:09 +0100

    Add files via upload

---
+++ src/main/java/csan/springframework/DependencyInjectionTestApplication.java
@@ -0,0 +1,12 @@
+package csan.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInjectionTestApplication.class, args);
+	}
+}
