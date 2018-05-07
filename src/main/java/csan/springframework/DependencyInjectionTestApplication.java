1f3cfe23b657b51462b2e7b1409a2966ebb3c6ea
codarsan <@eclipse>
2018-05-07 11:03:43 +0100

    YAML config

--- src/main/java/csan/springframework/DependencyInjectionTestApplication.java
+++ src/main/java/csan/springframework/DependencyInjectionTestApplication.java
@@ -7,17 +7,20 @@ import csan.springframework.controllers.ConstructorInjectedController;
 import csan.springframework.controllers.HelloController;
 import csan.springframework.controllers.PropertyInjectedController;
 import csan.springframework.controllers.SetterInjectedController;
+import csan.springframework.examplebeans.DataSource;
 
 @SpringBootApplication
 public class DependencyInjectionTestApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DependencyInjectionTestApplication.class, args);
-		//HelloController contr = (HelloController) ctx.getBean("helloController");
+		//HelloController controller = (HelloController) ctx.getBean("helloController");
 		//System.out.println(contr.Hello());
-		System.out.println(ctx.getBean(HelloController.class).Hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		//System.out.println(ctx.getBean(HelloController.class).Hello());
+		//System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		//System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+		//System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		DataSource dataSource = (DataSource) ctx.getBean(DataSource.class);
+		System.out.println(dataSource.getUsername());
 	}
 }
